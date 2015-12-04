4b221191a9c334727d8294cc801a3300fb0bae08
Sasini Madhumali <sasinim@IronOne.Local>
2015-12-04 15:25:16 +0530

    Profile Halfway

---
+++ Alumini_Assignment/Models/ProfileModels.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+
+namespace Alumini_Assignment.Models
+{
+    public class ProfileModels
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MobileNo { get; set; }
+        public string NicNo { get; set; }
+        public Image ProfilePicture { get; set; }
+        public string CurrentAddress { get; set; }
+        public string Occupation { get; set; }
+    }
+
+    public class ProposalDBContext : DbContext
+    {
+        public DbSet<ProfileModels> Movies { get; set; }
+    }
+}
