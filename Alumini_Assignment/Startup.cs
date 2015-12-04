381e8fa6f65e4fb062b893d1d8162696c3aa2acd
Buddhi Weerasinghe <buddhiw@IronOne.Local>
2015-12-04 11:05:13 +0530

    Initial Commit

---
+++ Alumini_Assignment/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Alumini_Assignment.Startup))]
+namespace Alumini_Assignment
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
